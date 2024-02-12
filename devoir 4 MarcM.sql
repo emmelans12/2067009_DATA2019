0b20d0f37c6c6a122d3a67f761cb4b6215eb854f
emmelans12 <marc.melanson@hotmail.ca>
2024-02-12 13:47:21 -0400

    Update devoir 4 MarcM.sql

--- devoir 4 MarcM.sql
+++ devoir 4 MarcM.sql
@@ -31,3 +31,17 @@ WHERE
 
 -- Exercise 3 (Obtenir la liste des noms d'auteurs ayant touché une avance supérieure à toutes les avances versées par l'éditeur "Harmattan".
 
+
+SELECT au_fname, au_lname
+FROM authors
+WHERE au_id IN (
+    SELECT au_id
+    FROM titleauthor AS ta
+    JOIN titles as t ON ta.title_id = t.title_id
+    WHERE advance > ALL (
+        SELECT advance
+        FROM titles AS t
+        JOIN publishers AS p ON t.pub_id = p.pub_id
+        WHERE pub_name = 'Harmattan'
+    )
+);
