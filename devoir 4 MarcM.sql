02d08dd7cf9ff1df6d4360091234281fff8c7b9b
emmelans12 <marc.melanson@hotmail.ca>
2024-02-12 13:49:24 -0400

    Update devoir 4 MarcM.sql

--- devoir 4 MarcM.sql
+++ devoir 4 MarcM.sql
@@ -2,15 +2,14 @@
 USE library;
 
 -- exercise 1 (Obtenir la liste des auteurs dont l’éditeur « Harmattan » n’a publié aucun livre)
-
-SELECT au_id, au_fname, au_lname
-FROM authors
-WHERE au_id NOT IN (
-    SELECT DISTINCT au_id
-    FROM titleauthor
-    INNER JOIN titles ON titleauthor.title_id = titles.title_id
-    INNER JOIN publishers ON titles.pub_id = publishers.pub_id
-    WHERE publishers.pub_name = 'Harmattan'
+SELECT DISTINCT a.au_fname, a.au_lname
+FROM authors a
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM titleauthor ta
+    JOIN titles t ON ta.title_id = t.title_id
+    JOIN publishers p ON t.pub_id = p.pub_id
+    WHERE p.pub_name = 'Harmattan' AND a.au_id = ta.au_id
 );
 
 -- Exercise 2 (Obtenir la liste des auteurs dont l’éditeur «Eyrolles » a publié tous les livres)
